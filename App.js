13b688a1d92cc2f4fec9f5a1333cae5d0885381d
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-27 15:39:28 -0300

    config-routes

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Task from './src/pages/Task/';
+import NewTask from './src/pages/NewTask/';
+import Details from './src/pages/Details/';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -17,16 +19,22 @@ export default function App() {
             headerTintColor: "#f92e6a"
           }}
         />
+        <Stack.Screen
+          name="NewTask"
+          component={NewTask}
+          options={{
+            headerTintColor: "#f92e6a"
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerTintColor: "#f92e6a"
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
